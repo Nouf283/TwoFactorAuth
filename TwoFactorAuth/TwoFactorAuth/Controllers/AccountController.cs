0862a657e9ab6c28efbf01323b53b406db1d1db0
Nouf283 <noufawal0311@gmail.com>
2023-05-20 02:03:50 +0600

    user and register added

---
+++ TwoFactorAuth/TwoFactorAuth/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TwoFactorAuth.Dtos;
+using TwoFactorAuth.Entities;
+
+namespace TwoFactorAuth.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser>userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if(user== null)
+            {
+                return Unauthorized();
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password,false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized();
+            }
+            return new UserDto
+            {
+                Email = loginDto.Email,
+                UserName = user.UserName,
+                Token = "this is a token"
+            };
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                Email = registerDto.Email,
+                UserName = registerDto.UserName,
+                FirstName = registerDto.UserName,
+            };
+           
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded)
+            {
+                return Unauthorized();
+            }
+            return new UserDto
+            {
+                Email = registerDto.Email,
+                UserName = user.UserName,
+                Token = "this is a token"
+            };
+        }
+    }
+}
