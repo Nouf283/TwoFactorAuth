16557d0bd7d3659515a68c1bb2c1c8bb38d41772
Nouf283 <noufawal0311@gmail.com>
2023-05-23 23:54:29 +0600

    lazy loading added

--- AngularLogin/src/app/app-routing.module.ts
+++ AngularLogin/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './Employee/employee-list/employee-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  // { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  // {
+  //   path: 'employee',
+  //   component: EmployeeListComponent,
+  // },
   {
-    path: 'employee',
-    component: EmployeeListComponent,
-  },
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
+  }
 ];
 
 @NgModule({
